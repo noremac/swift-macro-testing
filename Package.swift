e045332c981e250bfd1e472d3c13568204724212
Cameron Pulsford <cpulsfor@me.com>
2025-01-11 11:07:35 -0500

    wip

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.17.4"),
-    .package(url: "https://github.com/swiftlang/swift-syntax", "509.0.0"..<"601.0.0-prerelease"),
+    .package(url: "https://github.com/swiftlang/swift-syntax", from: "601.0.0-latest"),
   ],
   targets: [
     .target(
